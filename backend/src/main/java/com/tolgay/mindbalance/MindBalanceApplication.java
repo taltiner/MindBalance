7e31779e21adcb1ae32c0074639a662b9e02c2f3
Tolgay Altiner <tolgay.altiner@web.de>
2025-05-11 22:41:54 +0200

    Ordner Struktur angelegt

---
+++ backend/src/main/java/com/tolgay/mindbalance/MindBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.tolgay.mindbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindBalanceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tolgay/mindbalance/MindBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolgay.mindbalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
